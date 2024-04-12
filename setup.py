0ab28489fae380f7a6fb59059b3853f1ad0b4386
Kodlak15 <stanlcod15@gmail.com>
2024-04-11 22:04:22 -0700

    Undoing things I did

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
     version="v0.1.0-alpha", 
     description="Command line utility script for managing wallpaper with swww (https://github.com/LGFae/swww)", 
     packages=find_packages(), 
+    entry_points={
+        "console-scripts": ["swwwmgr = swwwmgr.main:main"],
+    },
     install_requires=[ 
         "pyyaml"
     ], 
