275508ab404b4210bdfa4c3c46547493f116c577
Kodlak15 <stanlcod15@gmail.com>
2024-04-11 21:05:42 -0700

    Undoing things I did

--- swwwmgr/__init__.py
+++ swwwmgr/__init__.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
-from pathlib import Path
-from shutil import copy
-import os
-
-CONFIG_PATH = Path.home().joinpath(".config", "swwwmgr", "config.yaml")
-
-if not CONFIG_PATH.exists():
-    dirname = Path(__file__).parent
-    src = dirname.joinpath("config.yaml")
-
-    if not CONFIG_PATH.parent.exists():
-        os.makedirs(CONFIG_PATH.parent)
-
-    copy(src, CONFIG_PATH)
+# from pathlib import Path
+# from shutil import copy
+# import os
+#
+# CONFIG_PATH = Path.home().joinpath(".config", "swwwmgr", "config.yaml")
+#
+# if not CONFIG_PATH.exists():
+#     dirname = Path(__file__).parent
+#     src = dirname.joinpath("config.yaml")
+#
+#     if not CONFIG_PATH.parent.exists():
+#         os.makedirs(CONFIG_PATH.parent)
+#
+#     copy(src, CONFIG_PATH)
