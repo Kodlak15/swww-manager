2bbbf0e1890cbec9805533fe89df9278819257ba
Kodlak15 <stanlcod15@gmail.com>
2024-04-11 21:18:36 -0700

    Undoing things I did

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
-from setuptools import setup 
+from setuptools import setup, find_packages
   
 setup( 
     name="swwwmgr", 
-    version='v0.1.0-alpha', 
+    version="v0.1.0-alpha", 
     description="Command line utility script for managing wallpaper with swww (https://github.com/LGFae/swww)", 
-    author='Cody Stanley', 
-    author_email="stanlcod15@protonmail.com", 
-    packages=["swwwmgr"], 
+    packages=find_packages(), 
     install_requires=[ 
         "pyyaml"
     ], 
