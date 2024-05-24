c31032fe7cf1875ed54ef8cd86391b3779758c51
Kodlak15 <stanlcod15@gmail.com>
2024-05-23 17:03:23 -0700

    Add random image option + more type annotations

--- swwwmgr/main.py
+++ swwwmgr/main.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
-import os
-import sys
 import argparse
+import os
+import random
 import subprocess
-import yaml
+import sys
 from pathlib import Path
+
+import yaml
 
 CONFIG_PATH = Path.home().joinpath(".config", "swwwmgr", "config.yaml")
 STATE_PATH = Path.home().joinpath(".local", "state", "swwwmgr", "state.yaml")
 
-def setup_config():
+
+def setup_config() -> None:
     if not os.path.exists(CONFIG_PATH.parent):
         os.makedirs(CONFIG_PATH.parent)
 
@@ -30,7 +33,8 @@ def setup_config():
         with open(CONFIG_PATH, "w") as f:
             yaml.dump(config, f)
 
-def setup_state():
+
+def setup_state() -> None:
     # Create the config and state directories if they do not exist
     print(STATE_PATH.parent)
     if not os.path.exists(STATE_PATH.parent):
@@ -45,30 +49,36 @@ def setup_state():
         with open(STATE_PATH, "w") as f:
             yaml.dump(state, f)
 
-def set_wallpaper(image: str, config: dict, state: dict):
+
+def set_wallpaper(image: str, config: dict, state: dict) -> None:
     # Use swww to change the wallpaper
-    subprocess.run([
-        "swww", 
-        "img", 
-        image,
-        "--transition-type",
-        config["transition"]["type"],
-        "--transition-pos",
-        config["transition"]["position"],
-        "--transition-step",
-        config["transition"]["step"],
-        "--transition-duration",
-        config["transition"]["duration"],
-        "--transition-angle",
-        config["transition"]["angle"],
-    ])
+    subprocess.run(
+        [
+            "swww",
+            "img",
+            image,
+            "--transition-type",
+            config["transition"]["type"],
+            "--transition-pos",
+            config["transition"]["position"],
+            "--transition-step",
+            config["transition"]["step"],
+            "--transition-duration",
+            config["transition"]["duration"],
+            "--transition-angle",
+            config["transition"]["angle"],
+        ]
+    )
 
     # If using pywal, run pywal using the new image to generate colors
     if config["pywal"]:
         try:
             subprocess.run(["wal", "-i", image, "-n"])
         except FileNotFoundError:
-            print("Could not find pywal, are you sure you have it installed?", file=sys.stderr)
+            print(
+                "Could not find pywal, are you sure you have it installed?",
+                file=sys.stderr,
+            )
 
     # Update the configuration
     directory = os.path.dirname(image)
@@ -78,48 +88,52 @@ def set_wallpaper(image: str, config: dict, state: dict):
     with open(STATE_PATH, "w") as f:
         yaml.dump(state, f)
 
-def set_directory(directory: str, config: dict, state: dict):
+
+def set_directory(directory: str, config: dict, state: dict) -> None:
     images = os.listdir(directory)
     image = os.path.join(directory, images[0])
     set_wallpaper(image, config, state)
 
-def next_image(config: dict, state: dict):
+
+def next_image(config: dict, state: dict) -> None:
     images = os.listdir(state["directory"])
-    image = os.path.join(
-        state["directory"], 
-        images[(state["index"] + 1) % len(images)]
-    )
+    image = os.path.join(state["directory"], images[(state["index"] + 1) % len(images)])
     set_wallpaper(image, config, state)
 
-def prev_image(config: dict, state: dict):
+
+def prev_image(config: dict, state: dict) -> None:
     images = os.listdir(state["directory"])
-    image = os.path.join(
-        state["directory"], 
-        images[(state["index"] - 1) % len(images)]
-    )
+    image = os.path.join(state["directory"], images[(state["index"] - 1) % len(images)])
     set_wallpaper(image, config, state)
+
+
+def random_image(config: dict, state: dict) -> None:
+    images = os.listdir(state["directory"])
+    image = os.path.join(state["directory"], images[random.randint(0, len(images))])
+    set_wallpaper(image, config, state)
+
 
 def get_config() -> dict:
     with open(CONFIG_PATH, "r") as f:
         return yaml.safe_load(f)
 
+
 def get_state() -> dict:
     with open(STATE_PATH, "r") as f:
         return yaml.safe_load(f)
 
-def main():
-    # Set up the configuration and state directories
-    setup_config()
-    setup_state()
 
-    # Command line arguments
+def get_args() -> argparse.Namespace:
+    """
+    Parse and return command line arguments
+    """
     parser = argparse.ArgumentParser(
         prog="swww_manager",
         description="Command line utility script for managing wallpaper with swww (https://github.com/LGFae/swww)",
     )
     parser.add_argument(
         "-i",
-        "--image", 
+        "--image",
         action="store",
         help="set wallpaper as the image at the provided path",
         metavar="",
@@ -127,7 +141,7 @@ def main():
     )
     parser.add_argument(
         "-d",
-        "--directory", 
+        "--directory",
         action="store",
         help="set a new active image directory",
         metavar="",
@@ -135,37 +149,56 @@ def main():
     )
     parser.add_argument(
         "-n",
-        "--next", 
+        "--next",
         action="store_true",
         help="set the wallpaper as the next image in the current active directory",
         required=False,
     )
     parser.add_argument(
         "-p",
-        "--prev", 
+        "--prev",
         action="store_true",
         help="set the wallpaper as the previous image in the current active directory",
         required=False,
     )
-    args = parser.parse_args()
-
-    # Get the configuration
-    config = get_config()
-    state = get_state()
+    parser.add_argument(
+        "-r",
+        "--random",
+        action="store_true",
+        help="set the wallpaper as a random image in the current active directory",
+        required=False,
+    )
 
     # If no arguments are passed, print the help message and exit
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
 
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    # Set up the configuration and state directories
+    setup_config()
+    setup_state()
+
+    # Get the configuration
+    config = get_config()
+    state = get_state()
+
     if args.image:
-        set_wallpaper(args.image, config, state) 
+        set_wallpaper(args.image, config, state)
     elif args.directory:
-        set_directory(args.directory, config, state) 
+        set_directory(args.directory, config, state)
+    elif args.random:
+        random_image(config, state)
     elif args.next:
         next_image(config, state)
     elif args.prev:
         prev_image(config, state)
+
 
 if __name__ == "__main__":
     main()
