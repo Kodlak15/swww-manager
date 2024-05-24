1f2df49e56241a18bc0b055f42483bcfc44de7c9
Kodlak15 <stanlcod15@gmail.com>
2024-05-23 17:25:25 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,50 +1,52 @@
 {
-  description = "Command line utility script for managing wallpaper with swww (https://github.com/LGFae/swww)";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    systems.url = "github:nix-systems/x86_64-linux";
+    nixvim.url = "github:nix-community/nixvim";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
 
   outputs = {
-    self,
     nixpkgs,
-  }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {inherit system;};
-        });
-  in {
-    devShells = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = [
-          (pkgs.python3.withPackages (python-pkgs: [
-            # Add packages here
-            python-pkgs.ipython
-            python-pkgs.pyyaml
-            python-pkgs.setuptools
-          ]))
-        ];
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells = {
+        default = pkgs.mkShell {
+          packages = [
+            (pkgs.python3.withPackages (python-pkgs: [
+              # Add packages here
+              python-pkgs.ipython
+              python-pkgs.pyyaml
+              python-pkgs.setuptools
+            ]))
+          ];
 
-        shellHook = ''
-          exec $SHELL;
-        '';
-      };
-    });
-
-    packages = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.python3.pkgs.buildPythonPackage rec {
-        pname = "swwwmgr";
-        version = "v0.1.0-alpha";
-
-        src = pkgs.fetchFromGitHub {
-          owner = "Kodlak15";
-          repo = "swww-manager";
-          rev = version;
-          hash = "sha256-4iJIzmHBJvRcz4aWqSAGHfYrCIi6v/PNF2ejgJ7LMJw=";
+          shellHook = ''
+            exec $SHELL;
+          '';
         };
+      };
 
-        propagatedBuildInputs = with pkgs.python311Packages; [pyyaml];
+      packages = {
+        default = pkgs.python3.pkgs.buildPythonPackage rec {
+          pname = "swwwmgr";
+          version = "v0.1.2-alpha";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "Kodlak15";
+            repo = "swww-manager";
+            rev = version;
+            hash = "sha256-wwz2OVV7MJkeqlTEUgeOobsCa3QKeiUu6DblUkhTaCk=";
+          };
+
+          propagatedBuildInputs = with pkgs.python311Packages; [pyyaml];
+        };
       };
     });
-  };
 }
