08dbfcb05d84f52d1cef1fe352680bd06dbc08bf
Kodlak15 <stanlcod15@protonmail.com>
2024-12-22 16:14:34 -0800

    Setting up hm module

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -34,13 +34,13 @@ in {
   };
   config = mkIf cfg.enable {
     # home.packages = [cfg.package];
-    # xdg.configFile.".config/swwwmgr/config.yaml".text = ''
-    #   transition:
-    #     angle: ${cfg.transition.angle}
-    #     duration: ${cfg.transition.duration}
-    #     position: ${cfg.transition.position}
-    #     step: ${cfg.transition.step}
-    #     type: ${cfg.transition.type}
-    # '';
+    xdg.configFile.".config/swwwmgr/config.yaml".text = ''
+      transition:
+        angle: ${cfg.transition.angle}
+        duration: ${cfg.transition.duration}
+        position: ${cfg.transition.position}
+        step: ${cfg.transition.step}
+        type: ${cfg.transition.type}
+    '';
   };
 }
